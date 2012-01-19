0cfae40360211a1dfac9c4c47d60335588c37dcf
John Williams <johndcwilliams@gmail.com>
2012-01-19 12:03:12 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+gem 'sqlite3'
+gem 'execjs'
+gem 'therubyracer'
+gem 'rack', '1.3.3'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+group :production do
+  gem 'pg'
+end
+
+group :development do
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'rspec'
+  gem 'webrat'
+  gem 'autotest'
+  gem 'redgreen'
+  gem 'test-unit'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adelphos::Application.config.secret_token = '77bd16da2799e21959f71202e8b9b86f8a363ec6fa932c747e7295503481117a39df33251f85ee6126bf81ed292b307442b5b58594d968dc9ff6a28c93bc57d1'
