00124232de53aeb33779045fa2fcf309066f28e1
John Williams <johndcwilliams@gmail.com>
2012-01-19 17:58:24 -0800

    Added stylesheet to production env.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,18 +10,20 @@ Adelphos::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
-
+  
   # Compress JavaScripts and CSS
   config.assets.compress = true
-
+  
+  # Fixes heroku error with stylesheets
+  config.assets.precompile += %w( blueprint/screen.css )
+  
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
   
-  # Fixes heroku error with stylesheets
-  config.assets.precompile += %w( *.css *.js )
+
   
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
