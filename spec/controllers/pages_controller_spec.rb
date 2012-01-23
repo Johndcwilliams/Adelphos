b47af0d55c04946c805e9ee688eb8e33a24db29c
John Williams <johndcwilliams@gmail.com>
2012-01-23 11:54:19 -0800

    Added layout links (with routs)

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,7 +12,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                       :content => "Adelphos Social Network | Home")
+                       :content => "#{@base_title} | Home")
     end
     
     it "should have a non-blank body" do
@@ -30,7 +30,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                   :content => "Adelphos Social Network | Contact")
+                   :content => "#{@base_title} | Contact")
     end    
   end
 
@@ -43,7 +43,20 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                       :content => "Adelphos Social Network | About")
+                       :content => "#{@base_title} | About")
+    end
+  end
+  
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                       :content => "#{@base_title} | Help")
     end
   end
 end
